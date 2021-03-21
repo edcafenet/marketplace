62db2855048d23cfea9ccfaee160a4692a1be94e
Eduardo Castello <edcafenet@gmail.com>
2021-03-21 23:33:37 +0100

    Added Splash Page

---
+++ docs/precache-manifest.a445ce7babc519fc77d9339be4500923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7e13f24f213571af1e",
+    "url": "/static/css/main.14c0eacc.chunk.css"
+  },
+  {
+    "revision": "3f7e13f24f213571af1e",
+    "url": "/static/js/main.3f7e13f2.chunk.js"
+  },
+  {
+    "revision": "aae3498a7a0c1adfdb58",
+    "url": "/static/css/1.0c1acca9.chunk.css"
+  },
+  {
+    "revision": "aae3498a7a0c1adfdb58",
+    "url": "/static/js/1.aae3498a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93b43597edecd18877b7959319c3be86",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b0724a7b02fb115e654180bfe34d876.js"
+  "/precache-manifest.a445ce7babc519fc77d9339be4500923.js"
 );
 
 workbox.clientsClaim();
